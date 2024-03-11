cbd58dfe27d1841bf59ea25f0989df4c632d454e
takeshi-fukasawa <fukasawa3431@gmail.com>
2024-03-11 10:45:02 +0900

    specrral_bound_func => spectral_func (rename)

--- spectral_func.m
+++ spectral_func.m
@@ -1,29 +1,162 @@
-function [x_sol_cell,other_output_k,iter_info]=...
+function [x_sol_cell,other_output_k_plus_1,iter_info]=...
     spectral_func(fun,spec,...
     x_0_cell,varargin)
 
-    n_var=size(x_0_cell,2);
+%%% Allow output of additional vars %%%%
+%%% Input %%%%
+% x1_0,x2_0,...: initial value
+% update_spec: dimension of a variable whose updating tune parameters  should be independently chosen (e.g. time)
+%%% update_spec==0 => Implement standard fixed point iteration
+% dampening_param
 
-    spec.x_max_cell=cell(1,n_var);
-    spec.x_min_cell=cell(1,n_var);
+n_var=size(x_0_cell,2);
 
-if isfield(spec,'SQUAREM_spec')==0
-    SQUAREM_spec=0;
-else
-    SQUAREM_spec=spec.SQUAREM_spec;
+run preliminary_spectral.m
+
+
+% varargin:1*XXX
+
+fun_k_cell={};
+
+%% Read inputs
+other_input_cell=varargin;
+
+bound_spec=0;
+for i=1:n_var
+   if isempty(x_max_cell{1,i})==0 | isempty(x_min_cell{1,i})==0
+     bound_spec=1;
+   end
 end
 
+
+DIST_table=NaN(ITER_MAX,n_var);
+alpha_table=NaN(ITER_MAX,n_var);
+ITER_table_LINE_SEARCH=NaN(ITER_MAX,1);
+
+FLAG_ERROR=[];
+
+
+tic
+ if bound_spec==1
+    x_0_cell=projection_func(x_0_cell,x_max_cell,x_min_cell);
+ end        
+ [fun_0_cell,other_output_0]=fun(x_0_cell{:},other_input_cell{:});
+
+feval=1;
+    
+    %%% DIST: sup norm of F(x)=x-Phi(x). 
+    DIST_vec=ones(1,n_var);
+    for i=1:n_var
+      DIST_vec(1,i)=max(abs(fun_0_cell{1,i}),[],'all','omitnan');
+    end
+
+    DIST=nanmax(DIST_vec);
+    DIST_table(1,:)=DIST_vec;
+    alpha_table(1,:)=alpha_0;
+
+    
+
+x_k_cell=x_0_cell;
+fun_k_cell=fun_0_cell;
+other_output_k_plus_1=other_output_0;
+
+%%%%%%%% Loop %%%%%%%%%%%
+
+if DIST>TOL
+for k=0:ITER_MAX-1
+
+
+   if k>=1
+      for i=1:n_var
+        Delta_x_cell{1,i}=x_k_cell{i}-x_k_minus_1_cell{i};
+        Delta_fun_cell{1,i}=fun_k_cell{i}-fun_k_minus_1_cell{i};
+      end % loop wrt i
+
+      if update_spec==0
+          for i=1:n_var
+              alpha_k{1,i}=1;
+          end
+      else
+        alpha_k=compute_alpha_func(...
+         Delta_x_cell,Delta_fun_cell,...
+            common_alpha_spec,compute_alpha_spec,dampening_param,update_spec);
+      end
+
+  else % k==0
+      for i=1:n_var
+       alpha_k{1,i}=alpha_0;
+     end% for loop wrt i
+   end
+
+     
+    %%% Update variables %%%%%%%%%%%%%%%
+    [x_k_plus_1_cell, fun_k_plus_1_cell,...
+    other_output_k_plus_1,DIST_vec,iter_line_search,alpha_vec]=...
+        spectral_update_func(fun,x_k_cell,alpha_k,fun_k_cell,other_input_cell,...
+        n_var,line_search_spec,...
+        DIST_table,ITER_MAX_LINE_SEARCH,bound_spec,...
+        x_max_cell,x_min_cell,k,max_opt_spec);
+
+    ITER_table_LINE_SEARCH(k+2,1)=iter_line_search;%% Number of line search iterations
+
+    feval=feval+iter_line_search;
+
+    DIST_table(k+2,:)=DIST_vec;
+    alpha_table(k+2,:)=alpha_vec;
+    DIST=nanmax(DIST_vec);
+
+    if isnan(DIST)==1|isinf(sum(DIST))==1|isnan(sum(DIST))==1
+       %warning("Error ?? ")
+       x_k_plus_1_cell=x_k_cell;
+       FLAG_ERROR=1;
+       break;
+    end
    
-   if SQUAREM_spec==0 
-       [x_sol_cell,other_output_k,iter_info]=...
-        spectral_bound_func(fun,spec,...
-        x_0_cell,varargin{:});
+   interval=10;
+    if k-floor(k/interval)*interval==0&DEBUG==1
+        DIST_vec
+    end
 
-    else
-[x_sol_cell,other_output_k,DIST_table,iter_info,fun_k_cell]=...
-        SQUAREM_func(fun,spec,...
-        x_0_cell,varargin{:});
+    if DIST<TOL
+        %DIST
+        FLAG_ERROR=0;
+        break;
+    end
+
+    %%% Replace variables for the next iteration
+	x_k_minus_1_cell=x_k_cell;
+	x_k_cell=x_k_plus_1_cell;
+	fun_k_minus_1_cell=fun_k_cell;
+    fun_k_cell=fun_k_plus_1_cell;
+   
+end %% end of for loop wrt k=0:ITER_MAX-1
+
+
+else % no iteration
+    k=0;
+    x_k_plus_1_cell=x_k_cell;
+    x_sol_cell=x_0_cell;
+    other_output_k_plus_1=other_output_0;
+    DIST_MAT=[];
+    fun_k_cell=fun_0_cell;
 end
 
+%% Output
+x_sol_cell=x_k_plus_1_cell;
 
-end
+t_cpu=toc;
+iter_info.t_cpu=t_cpu;
+iter_info.n_iter=k;
+iter_info.feval=feval;
+iter_info.ITER_MAX=ITER_MAX;
+iter_info.FLAG_ERROR=FLAG_ERROR;
+
+iter_info.fun_cell=fun_k_cell;
+
+iter_info.DIST_table=DIST_table;
+iter_info.alpha_table=alpha_table;
+iter_info.ITER_table_LINE_SEARCH=ITER_table_LINE_SEARCH;
+
+return
+
+
